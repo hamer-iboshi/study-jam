bef558771bdae00b789ea73e8883bbcfd58671ae
DEVELOPER <developer@LADMAC0220.local>
2017-04-29 17:29:40 -0300

    update android files of app

--- firtApp/app/src/main/java/com/example/aluno/firtapp/MainActivity.java
+++ firtApp/app/src/main/java/com/example/aluno/firtapp/MainActivity.java
@@ -5,23 +5,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
     private Button btnHi;
-
-
+    private TextView tviText;
+    private EditText eteMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(+R.layout.activity_main);
+        tviText = findViewById(R.id.tvi_text);
         btnHi = (Button) findViewById(R.id.btn_hi);
+        eteMessage = findViewById(R.id.ete_message);
         btnHi.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Log.i("Hello World!");
+                String message = eteMessage.getText().toString();
+                tviText.setText(message);
             }
         });
     }
